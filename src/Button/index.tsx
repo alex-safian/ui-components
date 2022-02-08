1670a7897c1974714bedff9e2f3cf1a2fd7fc8c3
Alex <alireza.safian@gmail.com>
2022-02-08 20:27:55 +0800

    Add chromatic

---
+++ src/Button/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Button as AntButton, ButtonProps as AntButtonProps } from "antd";
+
+import 'antd/lib/button/style/index.css'
+import "./style.css"
+
+export type ButtonProps = AntButtonProps;
+
+const Button: React.FC<ButtonProps> = (props) => {
+  return <AntButton {...props} />;
+};
+
+Button.propTypes = {};
+
+export default Button;
---
+++ src/Button/stories/index.stories.tsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button, {ButtonProps } from '../index'
+
+export default {
+  title: 'Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args: ButtonProps) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+	label: 'Button',
+	children: 'button'
+};
